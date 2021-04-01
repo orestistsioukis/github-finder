be93dba0fe8df7c77b9ca13ccc6f2d020d5a5008
Orestis Tsioukis <orestistsioukis@gmail.com>
2021-04-01 20:04:38 +0300

    classes with React and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<Navbar title='Github Finder' icon='fab fa-github' />
+				<Navbar />
+				<div className='container'>
+					<Users />
+				</div>
 			</div>
 		);
 	}
